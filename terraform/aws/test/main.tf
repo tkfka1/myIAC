0c796358f96756a34cff926e589f3353fe9bcf59
hankyo <36651040+tkfka1@users.noreply.github.com>
2024-06-10 20:33:40 +0900

    Update main.tf

--- terraform/aws/test/main.tf
+++ terraform/aws/test/main.tf
@@ -1,44 +1,27 @@
-resource "aws_instance" "example3" {
-  ami           = "ami-0b8414ae0d8d8b4cc"
-  instance_type = "t3.nano"
-  key_name = "hk_an2_rsa"
-  
-  root_block_device {
-    volume_size           = 30
-    volume_type           = "gp3"
-  }
-  
-  tags = {
-    Name = "terraform-example3"
-  }
-}
-	
-resource "aws_instance" "example4" {
-  ami           = "ami-0ff0fc8eddd393732"
-  instance_type = "t3.nano"
-  key_name = "hk_an2_rsa"
-  
-  root_block_device {
-    encrypted = true
-  }
-  
-  tags = {
-    Name = "terraform-example-end"
-  }
-}
 
-resource "aws_instance" "example4_old" {
-  ami           = "ami-01890a6b9be8ab599"
-  instance_type = "t3.nano"
+resource "aws_instance" "examplex" {
+  ami                    = "ami-0d6e6a06d11d7777d" # 변경된 AMI ID
+  instance_type          = "t3.nano" # 변경된 인스턴스 타입
+  subnet_id              = "subnet-0e28cce7fef64bc65" # 기존 서브넷 ID
   key_name = "hk_an2_rsa"
-  
-  root_block_device {
-    volume_size           = 30
-    volume_type           = "gp3"
-    encrypted = true
-  }
-  
+
   tags = {
-    Name = "terraform-example-end"
+    Name = "terraform-example3" # 변경된 태그
   }
+
+  lifecycle {
+    ignore_changes = [ ebs_block_device, user_data ]
+  }
+
+  root_block_device {
+    delete_on_termination = false
+    volume_size           = 11 # 변경된 볼륨 크기
+    volume_type           = "gp3"
+    encrypted             = true
+    iops                  = 3000
+    throughput            = 125
+    kms_key_id            = "arn:aws:kms:ap-northeast-2:082284794827:key/edbcabc8-f231-43c6-b254-1ac5eedfe565"
+  }
+
+  vpc_security_group_ids = ["sg-0485265aa6be0fa95"] # 기존 보안 그룹 ID
 }
