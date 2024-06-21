87ab71588702be2779c0038117845985aa410e07
hankyo <36651040+tkfka1@users.noreply.github.com>
2024-06-21 10:49:19 +0900

    Update main.tf

--- terraform/aws/test/main.tf
+++ terraform/aws/test/main.tf
@@ -1,27 +1,22 @@
+resource "aws_db_instance" "from_snapshot" {
+  identifier              = "restored-db-instance"
+  snapshot_identifier     = "your-snapshot-id"  # 복원할 스냅샷 ID를 입력하세요.
+  instance_class          = "db.t3.micro"
+  allocated_storage       = 20
+  storage_type            = "gp2"
+  db_subnet_group_name    = "your-db-subnet-group"  # 필요 시 서브넷 그룹 설정
+  vpc_security_group_ids  = ["your-security-group-id"]  # 필요 시 보안 그룹 설정
 
-resource "aws_instance" "examplex" {
-  ami                    = "ami-0d6e6a06d11d7777d" # 변경된 AMI ID
-  instance_type          = "t3.nano" # 변경된 인스턴스 타입
-  subnet_id              = "subnet-0e28cce7fef64bc65" # 기존 서브넷 ID
-  key_name = "hk_an2_rsa"
+  depends_on = [
+    aws_db_subnet_group.db_subnet_group,  # 서브넷 그룹이 있는 경우 종속성 추가
+  ]
+}
+
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name       = "your-db-subnet-group"
+  subnet_ids = ["subnet-12345678", "subnet-87654321"]  # 서브넷 ID를 입력하세요.
 
   tags = {
-    Name = "terraform-example3" # 변경된 태그
+    Name = "My DB subnet group"
   }
-
-  lifecycle {
-    ignore_changes = [ ebs_block_device, user_data ]
-  }
-
-  root_block_device {
-    delete_on_termination = false
-    volume_size           = 11 # 변경된 볼륨 크기
-    volume_type           = "gp3"
-    encrypted             = true
-    iops                  = 3000
-    throughput            = 125
-    kms_key_id            = "arn:aws:kms:ap-northeast-2:082284794827:key/edbcabc8-f231-43c6-b254-1ac5eedfe565"
-  }
-
-  vpc_security_group_ids = ["sg-0485265aa6be0fa95"] # 기존 보안 그룹 ID
 }
