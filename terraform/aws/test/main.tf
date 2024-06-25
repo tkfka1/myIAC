3f7bea32452a5fec57fafc073964cd23a461b4b6
hankyo <36651040+tkfka1@users.noreply.github.com>
2024-06-25 17:41:00 +0900

    Update main.tf

--- terraform/aws/test/main.tf
+++ terraform/aws/test/main.tf
@@ -1,22 +1,15 @@
-resource "aws_db_instance" "from_snapshot" {
-  identifier              = "restored-db-instance"
-  snapshot_identifier     = "your-snapshot-id"  # 복원할 스냅샷 ID를 입력하세요.
-  instance_class          = "db.t3.micro"
-  allocated_storage       = 20
-  storage_type            = "gp2"
-  db_subnet_group_name    = "your-db-subnet-group"  # 필요 시 서브넷 그룹 설정
-  vpc_security_group_ids  = ["your-security-group-id"]  # 필요 시 보안 그룹 설정
+module "db_instance" {
+  source  = "../modules/rds/db_instance"
 
-  depends_on = [
-    aws_db_subnet_group.db_subnet_group,  # 서브넷 그룹이 있는 경우 종속성 추가
-  ]
-}
+  identifier        = "my-aurora-cluster"
 
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name       = "your-db-subnet-group"
-  subnet_ids = ["subnet-12345678", "subnet-87654321"]  # 서브넷 ID를 입력하세요.
+  engine            = "aurora-mysql"
+  engine_version    = "5.7.mysql_aurora.2.12.2"
+  instance_class    = "db.t3.small"
 
-  tags = {
-    Name = "My DB subnet group"
-  }
+  snapshot_identifier = "test-snap"  # 복원할 스냅샷 ID
+
+
+  parameter_group_name = module.db_parameter_group
+
 }
